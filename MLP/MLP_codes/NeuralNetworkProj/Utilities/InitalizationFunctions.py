034283be4f23e656920e425ee9a254d2514fc1f8
Xianbo XU <Silas_X@outlook.com>
2022-01-22 15:32:05 +0100

    Delete He Initalization

--- MLP/MLP_codes/NeuralNetworkProj/Utilities/InitalizationFunctions.py
+++ MLP/MLP_codes/NeuralNetworkProj/Utilities/InitalizationFunctions.py
@@ -3,14 +3,3 @@ import numpy as np
 
 def normal_initializer(size1, size2):
     return np.random.normal(size=(size1, size2))
-
-# TO DO
-def heWeigth(n):
-    # calculate the range for the weights
-    std = sqrt(2.0 / n)
-    # generate random numbers
-    numbers = random(1000)
-    # scale to the desired range
-    scaled = numbers * std
-    # summarize
-    return scaled.mean()
