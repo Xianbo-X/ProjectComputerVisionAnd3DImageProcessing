fd06704d04bbbc67efc6f15231085c527899b193
Xianbo XU <Silas_X@outlook.com>
2022-01-22 14:49:20 +0100

    Finish - Sigmoid, ReLu, Sigmoid activation functions and pass test - Add abstract class for Lossfunction and activation, make backward only need chain_grad - Alter __call__ record last data in the abstract baes class. - Delete method gradient_batches and gradient_single from activation base class - Alter forward() function don't track any information - Alter backward() function to record grad data

--- MLP/MLP_codes/NeuralNetworkProj/Functions/LossFunctions.py
+++ MLP/MLP_codes/NeuralNetworkProj/Functions/LossFunctions.py
@@ -1,16 +1,18 @@
 import numpy as np
 from NeuralNetworkProj.Functions.ActivationFunctions import sigmoid
 
-class LossFunction():
-    def __init__(self,forward,gradient) -> None:
-        self.forward=forward
-        self.gradient=gradient
-        self.require_update=False
-    
-    def __call__(self, x,label):
-        return self.forward(x,label)
 
-def binary_cross_entropy(predicted_op,true_y,reduce="mean"):
+class LossFunction():
+    def __init__(self, forward, gradient) -> None:
+        self.forward = forward
+        self.gradient = gradient
+        self.require_update = False
+
+    def __call__(self, x, label):
+        return self.forward(x, label)
+
+
+def binary_cross_entropy(predicted_op, true_y, reduce="mean"):
     """
     Args:
         predicted_op: 2D array with shape (n_samples,1)
@@ -19,14 +21,15 @@ def binary_cross_entropy(predicted_op,true_y,reduce="mean"):
     Returns:
         Cross-entropy value
     """
-    ce=-true_y*np.log(predicted_op)-(1-true_y)*np.log(1-predicted_op)
-    if (reduce=="mean"):
-        ce=np.sum(ce,axis=0,keepdims=True)/ce.shape[0]
-    if (reduce=="sum"):
-        ce=np.sum(ce,axis=0,keepdims=True)
+    ce = -true_y*np.log(predicted_op)-(1-true_y)*np.log(1-predicted_op)
+    if (reduce == "mean"):
+        ce = np.sum(ce, axis=0, keepdims=True)/ce.shape[0]
+    if (reduce == "sum"):
+        ce = np.sum(ce, axis=0, keepdims=True)
     return ce
 
-def binary_cross_entropy_derivative(predicted_op,true_y,reduce="mean"):
+
+def binary_cross_entropy_derivative(predicted_op, true_y, reduce="mean"):
     """
     Args:
         predicted_op: 2D array with shape (n_samples,1)
@@ -35,21 +38,23 @@ def binary_cross_entropy_derivative(predicted_op,true_y,reduce="mean"):
     Returns:
         Derivative of binary-CE over x
     """
-    assert predicted_op.shape[1]==1, "(n_features,1) required, got "+str(predicted_op.shape)
-    gradient=(predicted_op-true_y)/(predicted_op*(1-predicted_op))
-    if (reduce=="mean"):
-        gradient=gradient/gradient.shape[0]
+    assert predicted_op.shape[1] == 1, "(n_features,1) required, got " + \
+        str(predicted_op.shape)
+    gradient = (predicted_op-true_y)/(predicted_op*(1-predicted_op))
+    if (reduce == "mean"):
+        gradient = gradient/gradient.shape[0]
     return gradient
 
-def binary_cross_entropy_sigmoid(predicted_op,true_y,reduce="mean"):
-    """
-    """
-    true_y=true_y.reshape(-1,1)
-    assert (true_y.shape[0]==predicted_op.shape[0])
-    return binary_cross_entropy(sigmoid(predicted_op),true_y,reduce)
-    
 
-def binary_cross_entropy_sigmoid_derivative(predicted_op,true_y,reduce="mean"):
+def binary_cross_entropy_sigmoid(predicted_op, true_y, reduce="mean"):
+    """
+    """
+    true_y = true_y.reshape(-1, 1)
+    assert (true_y.shape[0] == predicted_op.shape[0])
+    return binary_cross_entropy(sigmoid(predicted_op), true_y, reduce)
+
+
+def binary_cross_entropy_sigmoid_derivative(predicted_op, true_y, reduce="mean"):
     """
     Args:
         predicted_op: 2D array with shape (n_samples,1)
@@ -58,12 +63,45 @@ def binary_cross_entropy_sigmoid_derivative(predicted_op,true_y,reduce="mean"):
     Returns:
         Derivative of BCE(sigmoid(x)) over x
     """
-    true_y=true_y.reshape(-1,1)
-    assert (true_y.shape[0]==predicted_op.shape[0])
-    gradient=sigmoid(predicted_op)-true_y
-    if (reduce=="mean"):
-        gradient=gradient/gradient.shape[0]
-    if (reduce=="sum"):
-        gradient=gradient
+    true_y = true_y.reshape(-1, 1)
+    assert (true_y.shape[0] == predicted_op.shape[0])
+    gradient = sigmoid(predicted_op)-true_y
+    if (reduce == "mean"):
+        gradient = gradient/gradient.shape[0]
+    if (reduce == "sum"):
+        gradient = gradient
     return gradient
-    
+
+
+def cross_entropy(x, label: np.ndarray, reduce="mean"):
+    one_hot = np.identity(x.shape[1]).take(label,axis=0)
+    return cross_entropy_one_hot(x, one_hot, reduce)
+
+
+def cross_entropy_one_hot(x, onehot_label, reduce="mean"):
+
+    log_data = -np.log(x)
+    result = log_data[onehot_label.astype(bool)]
+    if reduce == "mean":
+        result = np.mean(result)
+    return result
+
+def cross_entropy_one_hot_derivative(x,onehot_label,reduce="mean"):
+    """"
+    """
+    # log_data=-np.log(x)
+    result=np.zeros_like(x)
+    # result[onehot_label.astype(bool)]=1/(log_data[onehot_label.astype(bool)])
+    result[onehot_label.astype(bool)]=-1/(x[onehot_label.astype(bool)])
+    # result=onehot_label/log_data
+    # result=-onehot_label/x
+    # TODO: solve zero log_data problem
+    if reduce=="mean":
+        result=result/x.shape[0]
+    if reduce=="none":
+        raise NotImplemented
+    return result
+
+def cross_entropy_derivative(x,label,reduce="mean"):
+    one_hot = np.identity(x.shape[1]).take(label,axis=0)
+    return cross_entropy_one_hot_derivative(x,one_hot,reduce)
