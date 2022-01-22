3f05a5a607b6a58a4b0ecd0fb9d2b793f4586f82
Xianbo XU <Silas_X@outlook.com>
2022-01-22 14:59:10 +0100

    Change name weights to weight, alter backward to only need chain_grad,let __call__ record last data
    Add gradient check in backward to avoid run backward more than once before updating

--- MLP/MLP_codes/NeuralNetworkProj/Layers/LinearLayer.py
+++ MLP/MLP_codes/NeuralNetworkProj/Layers/LinearLayer.py
@@ -5,24 +5,33 @@ class LinearLayer():
     y=xA^T+b
     """
     def __init__(self,input_size,output_size,initialization,bias=False) -> None:
-        self.weights=initialization(output_size,input_size)
+        self.weight=initialization(output_size,input_size)
         self.bias=None
+        self.trainable_variable_names=["weight"]
+        if bias:
+            self.trainable_variable_names.append("bias")
+            self.bias=initialization(1,output_size)
+
         self.lastData=None
         self.optimizers={}
         self.grad=None
         self.weight_grad=None
         self.bias_grad=None
         self.require_update=True
-        if bias:
-            self.bias=initialization(output_size,1)
     
-    def __call__(self, input):
-        return self.forward(input)
+        self.trainable_variable_seters={}
+        for name in self.trainable_variable_names:
+            self.trainable_variable_seters[name]=getattr(self,"set_"+name)
+    
+    def __call__(self, x):
+        self.lastData=x
+        return self.forward(self.lastData)
 
-    def set_weight(self,weights):
-        if weights.shape != self.weights.shape:
+    def set_weight(self,weight):
+        if weight.shape != self.weight.shape:
             raise BaseException("Shape Error")
-        self.weights=weights
+        self.weight=weight
+        self.weight_grad=None
     
     def set_bias(self, bias):
         if bias is False:
@@ -30,27 +39,34 @@ class LinearLayer():
         if bias.shape != self.bias.shape:
             raise BaseException("Shape Error")
         self.bias = bias
+        self.bias_grad=None
     
     def set_optimizer(self,optimizer,forwhom):
         self.optimizers[forwhom]=optimizer
         
 
     def forward(self,x):
-        self.lastData=x
-        result=x@self.weights.T
+        return self.linear_transform(x)
+    
+    def linear_transform(self,x):
+        result=x@self.weight.T
         if not (self.bias is None):
             result=result+self.bias
         return result
 
-    def backward(self,chain_grad,data=None):
-        if data is None:
-            data=self.lastData
+    def backward(self,chain_grad):
+        if not ((self.weight_grad is None) or (self.bias_grad is None)):
+            raise ("Can't backward twice before update parameters")
+
+        data=self.lastData
+        # if data is None:
+            # data=self.lastData
         self.derivative_on_weight(chain_grad,data=data)
         if not (self.bias is None):
             self.derivative_on_bias(chain_grad,data=data)
         self.gradient(chain_grad,data=data)
         return self.grad
-    
+
     def gradient(self,chain_grad,data=None):
         if data is None:
             data=self.lastData
@@ -61,7 +77,7 @@ class LinearLayer():
         return self.derivative_on_input_batches(chain_grad,data)
     
     def derivative_on_input_batches(self,chain_grad,data):
-        return chain_grad@self.weights
+        return chain_grad@self.weight
 
     def derivative_on_weight(self,chain_grad,reduce="sum",data=None):
         if data is None:
@@ -103,8 +119,8 @@ class LinearLayer():
             self.update_bias(chain_grad,*arg)
     
     def update_weights(self,chain_grad,*arg):
-        weights_grad=self.derivative_on_weight(chain_grad,"mean")
-        self.set_weight(self.optimizers["weights"](self.weights,weights_grad,*arg))
+        weight_grad=self.derivative_on_weight(chain_grad,"mean")
+        self.set_weight(self.optimizers["weights"](self.weight,weight_grad,*arg))
 
     def update_bias(self,chain_grad,*arg):
         bias_grad=self.derivative_on_bias(chain_grad,"mean")
